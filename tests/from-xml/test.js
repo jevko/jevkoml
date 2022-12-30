cd5b1735243ef7f06f4b249072589e0451555e41
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-30 20:14:13 +0100

    add test for conversion from and back to xml/html

---
+++ tests/from-xml/test.js
@@ -0,0 +1,15 @@
+import {fromXmlStr} from '../../fromXmlStr.js'
+import {jevkoStrToHtmlStr} from '../../jevkoml.js'
+import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
+import * as path from "https://deno.land/std@0.57.0/path/mod.ts";
+
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url))
+
+Deno.test('from xml', async () => {
+  const source = Deno.readTextFileSync(__dirname + '/sample.xml')
+  const jevkoStr = fromXmlStr(source)
+  const htmlStr = await jevkoStrToHtmlStr(jevkoStr)
+  // console.log(jevkoStr)
+  // console.log(htmlStr)
+  assertEquals(source, htmlStr)
+})
