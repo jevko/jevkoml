ccfd693ca0eb32220d740ae6224f917bd83afdc9
Darius J Chuck <darius.j.chuck+github@gmail.com>
2023-01-02 23:25:05 +0100

    fix translation of self-closing tags

--- fromXmlStr.js
+++ fromXmlStr.js
@@ -1,6 +1,19 @@
 // todo: deps.js, mod.js, etc.
-import {fnlxml, resolveEntity} from 'https://raw.githubusercontent.com/xtao-org/fnlxml/v0.2.0/fnlxml.js'
+import {fnlxml} from 'https://raw.githubusercontent.com/xtao-org/fnlxml/v0.2.0/fnlxml.js'
 import {escape} from 'https://cdn.jsdelivr.net/gh/jevko/jevko.js@v0.1.5/mod.js'
+
+// todo: handle HTML entities & hex entities & whathaveyou
+export const resolveEntity = (ent) => {
+  if (ent === '&lt;') return '<'
+  if (ent === '&gt;') return '>'
+  if (ent === '&quot;') return '"'
+  if (ent === '&apos;') return "'"
+  if (ent === '&amp;') return "&"
+  if (ent === '&nbsp;') return " "
+  if (ent === '&#39;') return "\x39"
+  throw Error(`Unknown entity: ${ent}`)
+}
+
 
 export const fromXmlStr = (str) => {
   let ret = ''
@@ -13,6 +26,7 @@ export const fromXmlStr = (str) => {
       else if (name === 'Reference') ret += resolveEntity(str)
       else if (name === 'Attribute') ret += `]`
       else if (name === 'ETagName') ret += `]`
+      else if (name === 'EETagC') ret += `]`
       else if (name === 'CData') ret += str
       else if (name === 'CharData') ret += str
 
